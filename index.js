7e5ab0bb009c8185dc79d7c109134e916f32b35e
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-17 17:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  console.log("Happy ${holiday}, ${name}!")
-  return 'It\'s 20 days until Mother's Day!'
+  console.log("It\'s ${days} days until ${holiday}!")
+  return 'It\'s 20 days until Mother\'s Day!'
 }
